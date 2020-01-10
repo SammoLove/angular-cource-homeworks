7adda211d176e9490c03cfa57e6b1b135c87e1a7
main.SammoLove@gmail.com <SamSam11>
2020-01-10 04:57:28 +0200

    HW2 Init

---
+++ hotels-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListComponent } from './list/list.component';
+import { WeatherComponent } from './weather/weather.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListComponent,
+    WeatherComponent,
+    ProfileComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
